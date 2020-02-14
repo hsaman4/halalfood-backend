982dd79ef3c424f154f62d57853cd08d8f19f322
Hadeeqa Saman <hadeeqasaman95@gmail.com>
2020-02-13 22:56:48 -0500

    tryin to deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://halal-foodies.herokuapp.com/'
 
     resource '*',
       headers: :any,
